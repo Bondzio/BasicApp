6cdcbdebba583e4a028fac6de4b1492aa5a7e464
ritu.joshi <ritu@hotmail.de>
2019-07-22 22:04:03 +0200

    Start Image and Table of Content

--- app/src/main/java/com/example/basicapp/MainActivity.java
+++ app/src/main/java/com/example/basicapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.basicapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;              // time to show the starting symbol is 4 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run (){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);  //to go to the next activity
+                startActivity(homeIntent);
+                finish();
+        }
+    }, SPLASH_TIME_OUT);
+}
 }
