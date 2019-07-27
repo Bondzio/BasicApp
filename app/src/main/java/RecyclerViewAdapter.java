afb58aea91915044a788f071b7bc23f22f1df9a8
Shubham Bansal <shubham.bansal@goeuro.com>
2019-07-27 20:41:23 +0200

    master changes

--- app/src/main/java/RecyclerViewAdapter.java
+++ app/src/main/java/RecyclerViewAdapter.java
@@ -22,70 +22,63 @@ import java.util.ArrayList;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private static final String TAG = "RecyclerViewAdapter";
+	private static final String TAG = "RecyclerViewAdapter";
 
-    private ArrayList<String> mImageNames = new ArrayList<>();
-    private ArrayList<String> mImages = new ArrayList<>();
-    private Context mContext;
+	private ArrayList<String> mImageNames = new ArrayList<>();
+	private ArrayList<String> mImages = new ArrayList<>();
+	private Context mContext;
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images ) {
-        mImageNames = imageNames;
-        mImages = images;
-        mContext = context;
-    }
+	public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images) {
+		mImageNames = imageNames;
+		mImages = images;
+		mContext = context;
+	}
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
-    }
+	@Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+		ViewHolder holder = new ViewHolder(view);
+		return holder;
+	}
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        Log.d(TAG, "onBindViewHolder: called.");
+	@Override public void onBindViewHolder(ViewHolder holder, final int position) {
+		Log.d(TAG, "onBindViewHolder: called.");
 
-        Glide.with(mContext)
-                .asBitmap()
-                .load(mImages.get(position))
-                .into(holder.image);
+		Glide.with(mContext).asBitmap().load(mImages.get(position)).into(holder.image);
 
-        holder.imageName.setText(mImageNames.get(position));
+		holder.imageName.setText(mImageNames.get(position));
 
-       holder.parentLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+		holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View view) {
+				Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
 
-                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
+				Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(mContext, ListDisplay.class);
-                intent.putExtra("subject", Subjects.values());
-                mContext.startActivity(intent);
-            }
-        });
-    }
+				Intent intent = new Intent(mContext, ListDisplay.class);
+				intent.putExtra("subject", Subjects.values()[position]);
+				mContext.startActivity(intent);
+			}
+		});
+	}
 
 
-    @Override
-    public int getItemCount() {
-        return mImageNames.size();
-    }
+	@Override public int getItemCount() {
+		return mImageNames.size();
+	}
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+	public class ViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView image;
-        TextView imageName;
-        RelativeLayout parentLayout;
+		CircleImageView image;
+		TextView imageName;
+		RelativeLayout parentLayout;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            image = itemView.findViewById(R.id.image);
-            imageName = itemView.findViewById(R.id.image_name);
-            parentLayout = itemView.findViewById(R.id.parent_layout);
-        }
-    }
+		public ViewHolder(View itemView) {
+			super(itemView);
+			image = itemView.findViewById(R.id.image);
+			imageName = itemView.findViewById(R.id.image_name);
+			parentLayout = itemView.findViewById(R.id.parent_layout);
+		}
+	}
 }
--- app/src/main/java/com/example/basicapp/ListDisplay.java
+++ app/src/main/java/com/example/basicapp/ListDisplay.java
@@ -7,19 +7,48 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListDisplay extends Activity {
-    String[] SubSectionsInformatik = {"2.1 Basis-Grundlagen","2.2 Theoretische Informatik",
-            "2.3. Technische Informatik ","2.4. Bioinformatik"};
-    String[] SubSectionsMathematik = {"1.1 Basis-Grundlagen","1.2 Theoretische Informatik",
-            "1.3. Technische Informatik ","1.4. Bioinformatik"};
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_listview, SubSectionsInformatik);
+	private Subjects selectedSubject;
 
-        ListView listView = (ListView) findViewById(R.id.InformatikSubSections);
-        listView.setAdapter(adapter);
-}
+	String[] SubSectionsInformatik = {"2.1 Basis-Grundlagen", "2.2 Theoretische Informatik",
+		"2.3. Technische Informatik ", "2.4. Bioinformatik"};
+
+	String[] SubSectionsMathematik = {"1.1 Basis-Grundlagen", "1.2 Theoretische Informatik",
+		"1.3. Technische Informatik ", "1.4. Bioinformatik"};
+
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// This is how one can pass data between activities
+		//TODO You can read more about Serializable
+		selectedSubject = (Subjects) getIntent().getSerializableExtra("subject");
+
+		ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_listview, displayList());
+		ListView listView = (ListView) findViewById(R.id.InformatikSubSections);
+		listView.setAdapter(adapter);
+	}
+
+
+	private String[] displayList() {
+
+		switch (selectedSubject) {
+			case Mathematik:
+				return SubSectionsMathematik;
+			case Informatik:
+				return SubSectionsInformatik;
+			case Biologie:
+				break;
+			case Chemie:
+				break;
+			case Physik:
+				break;
+			case Technik:
+				break;
+		}
+
+		return new String[0];
+	}
+
+
 }
