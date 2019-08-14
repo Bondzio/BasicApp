025d8e43bf836a634cb3360a1bbc3529b3c4c0e4
ritu.joshi <ritu@hotmail.de>
2019-08-14 23:38:18 +0200

    complete

--- app/src/main/java/com/example/basicapp/ui/SplashActivity.java
+++ app/src/main/java/com/example/basicapp/ui/SplashActivity.java
@@ -9,7 +9,7 @@ import com.example.basicapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-	private static int SPLASH_TIME_OUT = 2000;              // time to show the starting symbol is 4 seconds
+	private static int SPLASH_TIME_OUT = 4000;              // time to show the starting symbol is 4 seconds
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
